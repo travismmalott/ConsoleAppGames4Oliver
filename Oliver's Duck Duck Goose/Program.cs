f6fdc732a1214b23f962da44a7f786d52949ff51
Travis <56692346+travismmalott@users.noreply.github.com>
2020-01-29 08:38:22 -0600

    Oliver's Duck Duck Goose Game

---
+++ Oliver's Duck Duck Goose/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oliver_s_Duck_Duck_Goose
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            for (x = 0; x <= 7; x++)
+            {
+                Console.WriteLine("Duck");
+            }
+            if (x == 7)
+            {
+                x++;
+            }
+            if (x == 8)
+            {
+                Console.WriteLine("Goose");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
